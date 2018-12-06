cda07cefd3c69e07834b26345ee4bde44d1edbb1
João Paulo Relógio <joaoprelogio@gmail.com>
2018-12-06 23:26:54 +0000

    Create component post-list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { MatInputModule, MatCardModule, MatButtonModule } from '@angular/material';
+import { MatInputModule, MatCardModule, MatButtonModule, MatToolbarModule, MatExpansionModule } from '@angular/material';
+import { from } from 'rxjs';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PostCreateComponent } from './posts/post-create/post-create.component';
-import { from } from 'rxjs';
 import { HeaderComponent } from './header/header.component';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { PostsListComponent } from './posts/posts-list/posts-list.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
     PostCreateComponent,
-    HeaderComponent
+    PostsListComponent
   ],
   imports: [
     BrowserModule,
@@ -23,7 +27,9 @@ import { HeaderComponent } from './header/header.component';
     BrowserAnimationsModule,
     MatInputModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
