9023332ebcbd5f0890c06d01a93e3ed35a954066
João Paulo Relógio <joaoprelogio@gmail.com>
2018-12-22 23:37:19 +0000

    Passing ngForm to ReactiveForm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import {
   MatInputModule,
   MatCardModule,
@@ -28,7 +28,7 @@ import { PostsListComponent } from './posts/posts-list/posts-list.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
+    ReactiveFormsModule,
     BrowserAnimationsModule,
     MatInputModule,
     MatCardModule,
